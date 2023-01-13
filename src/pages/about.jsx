5c572c8f017e2bfd4e1d358ce0fa6666b1d13e68
ElPardo16 <andresito105@gmail.com>
2023-01-12 21:50:08 -0500

    about

---
+++ src/pages/about.jsx
@@ -0,0 +1,29 @@
+import Button from '../components/Button'
+import Layout from '../components/Layout'
+
+export default function About() {
+	return (
+		<>
+			<Layout title="about" about={true}>
+				<div className="about">
+					<div className="bio">
+						<img src="/img/osoColorDev.png" alt="" />
+						<h1>Andres Felipe Gonzalez Silva</h1>
+						<p className="big">Desarrollador</p>
+					</div>
+					<div className="des">
+						<div className="des_con">
+							<h2>¿Quien soy?</h2>
+							<p>Soy una persona que le gusta la programacion, desde los 13 años estoy en este gremio, y cada dia busco mejorar mis habilidades blandas y tecnicas</p>
+						</div>
+						<div className="des_con">
+							<h2>Curriculum</h2>
+							<p>Puedes descargar mi curriculum a continuacion</p>
+							<Button text="Decargar CV" />
+						</div>
+					</div>
+				</div>
+			</Layout>
+		</>
+	)
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
 		<Layout home={true}>
 			<div className="home">
 				<h1>Bienvenido a mi <br/>portafolio</h1>
-				<p className='big'>Veras de lo que puedo hacer y conoceras mas sobre mi</p>
+				<p className='big'>Veras lo que puedo hacer y conoceras mas sobre mi</p>
 				<Button text="Vamos"/>
 			</div>
 		</Layout>
